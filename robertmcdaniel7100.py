fd79a49b77290a5b645d0b394182a4b2bc5ff7b5
michalfinn7051 <146064701+michalfinn7051@users.noreply.github.com>
2023-10-02 09:29:01 +0530

    Update robertmcdaniel7100.py

--- robertmcdaniel7100.py
+++ robertmcdaniel7100.py
@@ -2,55 +2,23 @@ from mega import Mega
 import threading
 import time
 
-# Define a counter for successful renames
 successful_renames = 0
 
 def rename_files(mega, files_to_rename):
-    global successful_renames  # Declare the global counter variable
+    global successful_renames
     for file_name in files_to_rename:
-        new_name = file_name
-        
-        patterns_to_replace = {
-            "Telegram @OnlyShare0 - - ": "",
-            "Telegram @OnlyShare0 - -": "",
-            "Telegram @OnlyShare0 -- ": "",
-            "Telegram @OnlyShare0 --": "",
-            "Telegram @OnlyShare0 - ": "",
-            "Telegram @OnlyShare0 -": "",
-            "Telegram @OnlyShare0 ": "",
-            "Telegram @OnlyShare0": "",
-            "@Newsociety70 Telegram Join Channel ": "",
-            "@Newsociety70 Telegram Join Channel": "",
-            "_@newsociety0 telegram Fsociety_TG Reddit_ ": "",
-            "_@newsociety0 telegram Fsociety_TG Reddit_": "",
-            "@Nud3Cloud Telegram ": "",
-            "@Nud3Cloud Telegram": "",
-            "Fsociety_TG SubReddit Download Join ": "",
-            "Fsociety_TG SubReddit Download Join": "",
-        }
-        
-        if any(pattern in file_name for pattern in patterns_to_replace):
-            for pattern, replacement in patterns_to_replace.items():
-                new_name = new_name.replace(pattern, replacement)
-
-            file = mega.find(file_name)
-            try:
-                mega.rename(file, new_name)
-                print(f"{file_name} --> {new_name}")
-
-                successful_renames += 1
-            except:
-                try:
-                    mega.rename(file, new_name)
-                    print(f"{file_name} --> {new_name}")
-
-                    successful_renames += 1
-                except:
-                    print(f"Failed to rename {file_name}")   
+        new_name = f"@nsfwpack0 Telegram {file_name}"
+        file = mega.find(file_name)
+        try:
+            mega.rename(file, new_name)
+            print(f"{file_name} --> {new_name}")
+            successful_renames += 1
+        except Exception as e:
+            print(f"Failed to rename {file_name}: {e}")  
 
 def main():
     mega = Mega()
-    m = mega.login("robertmcdaniel7100@fexpost.com", "Bot@7051")
+    m = mega.login("elizabethsmith4269@fexpost.com", "Bot@7051")
 
     files = m.get_files()
     extracted = []
@@ -61,12 +29,10 @@ def main():
                 extracted.append(files[x]['a']['n'])
             except:
                 pass
-        
-    dump_txt = ["Telegram @OnlyShare0 - - ", "Telegram @OnlyShare0 - -", "Telegram @OnlyShare0 --", "Telegram @OnlyShare0 -", "Telegram @OnlyShare0", "@Newsociety70 Telegram Join Channel", "_@newsociety0 telegram Fsociety_TG Reddit_", "Fsociety_TG SubReddit Download Join", "@Nud3Cloud Telegram"]
-    matches = [txt for txt in extracted if any(keyword in txt for keyword in dump_txt)]
-    print(f"Total :- {len(matches)}")
+    extracted = extracted[(len(extracted)//2):]
+    print(f"Total :- {len(extracted)}")
     time.sleep(1)
-    num_threads = 100
+    num_threads = 50
     files_per_thread = len(extracted) // num_threads
     threads = []
 
@@ -83,7 +49,7 @@ def main():
         thread.join()
 
     # Print the total number of successful renames
-    print(f"Successful Renames: {successful_renames}/{len(matches)}")
+    print(f"Successful Renames: {successful_renames}/{len(extracted)}")
 
 if __name__ == "__main__":
     main()
